52e37f1be7c3d8695b410b21e0b9a4018508967c
Angelo Dejaeghere <angelo.dejaeghere@allphi.eu>
2021-09-21 09:34:07 +0200

    NTier DAL Basic

---
+++ G3L.Examples/G3L.Examples.NTier.DAL/Database/NTierDbContext.cs
@@ -0,0 +1,18 @@
+﻿using G3L.Examples.NTier.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace G3L.Examples.NTier.DAL.Database
+{
+    public class NTierDbContext : DbContext
+    {
+        public NTierDbContext()
+        {
+            
+        }
+        
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Visitor> Visitors { get; set; }
+        public DbSet<Visit> Visits { get; set; }
+    }
+}
---
+++ G3L.Examples/G3L.Examples.NTier.DAL/Entities/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace G3L.Examples.NTier.DAL.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+    }
+}
